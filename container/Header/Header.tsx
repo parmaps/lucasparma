c8eb9ce5c69d31721d356a75a14a00e56e5a3a67
donmerengue <l.parma@hotmail.com>
2022-10-09 21:24:28 -0300

    header: social icons + framer motion

--- container/Header/Header.tsx
+++ container/Header/Header.tsx
@@ -1,10 +1,48 @@
 import React from "react";
 import "./Header.module.scss";
+import { SocialIcon } from "react-social-icons";
+import { Box, Flex, Spacer } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 function Header({}: Props) {
-  return <div>Header</div>;
+  return (
+    <Flex maxW="960px" mx="auto" px="auto">
+      <Box
+        as={motion.div}
+        initial={{ x: -500, opacity: 0, scale: 0.5 }}
+        animate={{ x: 0, opacity: 1, scale: 1 }}
+        transition="1.5s ">
+        {/* Social Icons */}
+        <SocialIcon
+          url="https://www.linkedin.com/in/lucas-parma/"
+          fgColor="white"
+        />
+        <SocialIcon
+          url="https://github.com/parmaps/"
+          fgColor="white"
+          bgColor="transparent"
+        />
+      </Box>
+      <Spacer />
+      <Box
+        as={motion.div}
+        initial={{ x: 500, opacity: 0, scale: 0.75 }}
+        animate={{ x: 0, opacity: 1, scale: 1 }}
+        transition="1.5s ">
+        Header
+      </Box>
+
+      {/* <Flex>
+        <SocialIcon
+          network="email"
+          fgColor="white"
+          bgColor="transparent"
+        />
+      </Flex> */}
+    </Flex>
+  );
 }
 
 export default Header;
