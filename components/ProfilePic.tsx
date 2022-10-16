acbfbab2048dbe95f05bf0074522ec8f7281d7a3
donmerengue <l.parma@hotmail.com>
2022-10-16 17:38:43 -0300

    testing delete image motion

--- components/ProfilePic.tsx
+++ components/ProfilePic.tsx
@@ -8,15 +8,16 @@ type Props = {};
 function ProfilePic({}: Props) {
   return (
     <Box
-      as={motion.div}
-      w={[192, 240, 300]}
-      mx="auto"
-      mt={[2.5,5]}
-      mb={[5, 10]}
+    w={[192, 240, 300]}
+    mx="auto"
+    mt={[2.5,5]}
+    mb={[5, 10]}
+    /* as={motion.div}
       initial={{ opacity: 0, scale: 0.5 }}
       animate={{ opacity: 1, scale: 1 }}
       whileHover={{ scale: 1.05 }}
-      transition="2s">
+      transition="2s" */
+      >
       <Image
         alt="profile"
         src="/lucas-parma-profile.png"
