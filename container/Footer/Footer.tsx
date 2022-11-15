36131e5154ac40526b8ebcf378769720b6eadcaa
donmerengue <l.parma@hotmail.com>
2022-11-15 12:16:00 -0300

    descargar cv

--- container/Footer/Footer.tsx
+++ container/Footer/Footer.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Flex } from "@chakra-ui/react";
 import styles from "./Footer.module.scss";
-import SocialIconsFooter from "./SocialIcons";
+import SocialIconsFooter from "./SocialIconsFooter";
+import Link from "next/link";
 
 type Props = {};
 
@@ -10,10 +11,17 @@ const Footer = (props: Props) => {
     <Flex
       as="footer"
       align="center"
-      justify="center"
+      justify="space-around"
       height="100px"
       maxH="100px"
       className={styles.footer}>
+      <a
+        href="/files/cv-lucas-parma.pdf"
+        // target="_blank"
+        rel="noopener noreferrer"
+        download>
+        <p className={` ${styles.cv} ${styles.cv_hover} `}>Descargar CV</p>
+      </a>
       <SocialIconsFooter />
     </Flex>
   );
